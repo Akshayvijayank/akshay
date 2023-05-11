e6a9c60be7878034d22b94c58215d623d64e38dd
Akshayvijayank <125250654+Akshayvijayank@users.noreply.github.com>
2023-05-11 16:07:50 +0530

    Add files via upload
    two new files added one js and another css

---
+++ docs/index.js
@@ -0,0 +1,95 @@
+let pageone = document.getElementById("1")
+let pagetwo = document.getElementById("2")
+let pagethree = document.getElementById("3")
+let pagefour = document.getElementById("4")
+let pagefive = document.getElementById("5")
+let pagesix = document.getElementById("6")
+let pageseven = document.getElementById("7")
+let pageseight = document.getElementById("8")
+function display1(){
+    pageone.style.visibility = "visible"
+    pagetwo.style.visibility = "hidden"
+    pagethree.style.visibility = "hidden"
+    pagefour.style.visibility = "hidden"
+    pagefive.style.visibility="hidden"
+    pagesix.style.visibility = "hidden"
+    pageseven.style.visibility="hidden"
+    pageseight.style.visibility="hidden"
+}
+
+function display2(){
+    pageone.style.visibility = "hidden"
+    pagetwo.style.visibility = "visible"
+    pagethree.style.visibility = "hidden"
+    pagefour.style.visibility = "hidden"
+    pagefive.style.visibility="hidden"
+    pagesix.style.visibility = "hidden"
+    pageseven.style.visibility="hidden"
+    pageseight.style.visibility="hidden"
+}
+
+function display3(){
+    pageone.style.visibility = "hidden"
+    pagetwo.style.visibility = "hidden"
+    pagethree.style.visibility = "visible"
+    pagefour.style.visibility = "hidden"
+    pagefive.style.visibility="hidden"
+    pagesix.style.visibility = "hidden"
+    pageseven.style.visibility="hidden"
+    pageseight.style.visibility="hidden"
+}
+
+function display4(){
+    pageone.style.visibility = "hidden"
+    pagetwo.style.visibility = "hidden"
+    pagethree.style.visibility = "hidden"
+    pagefour.style.visibility = "visible"
+    pagefive.style.visibility="hidden"
+    pagesix.style.visibility = "hidden"
+    pageseven.style.visibility="hidden"
+    pageseight.style.visibility="hidden"
+}
+
+function display5(){
+    pageone.style.visibility = "hidden"
+    pagetwo.style.visibility = "hidden"
+    pagethree.style.visibility = "hidden"
+    pagefour.style.visibility = "hidden"
+    pagefive.style.visibility="visible"
+    pagesix.style.visibility = "hidden"
+    pageseven.style.visibility="hidden"
+    pageseight.style.visibility="hidden"
+}
+
+function display6(){
+    pageone.style.visibility = "hidden"
+    pagetwo.style.visibility = "hidden"
+    pagethree.style.visibility = "hidden"
+    pagefour.style.visibility = "hidden"
+    pagefive.style.visibility="hidden"
+    pagesix.style.visibility = "visible"
+    pageseven.style.visibility="hidden"
+    pageseight.style.visibility="hidden"
+}
+
+function display7(){
+    pageone.style.visibility = "hidden"
+    pagetwo.style.visibility = "hidden"
+    pagethree.style.visibility = "hidden"
+    pagefour.style.visibility = "hidden"
+    pagefive.style.visibility="hidden"
+    pagesix.style.visibility = "hidden"
+    pageseven.style.visibility="visible"
+    pageseight.style.visibility="hidden"
+}
+
+function display8(){
+    pageone.style.visibility = "hidden"
+    pagetwo.style.visibility = "hidden"
+    pagethree.style.visibility = "hidden"
+    pagefour.style.visibility = "hidden"
+    pagefive.style.visibility="hidden"
+    pagesix.style.visibility = "hidden"
+    pageseven.style.visibility="hidden"
+    pageseight.style.visibility="visible"
+}
